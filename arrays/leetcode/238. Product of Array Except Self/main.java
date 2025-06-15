0e7f9251b1177957e11ea184d451d40a488c2d67
shaswinayyan <shaswinayyan@gmail.com>
2025-06-15 23:07:08 +0530

    product of an array except self

---
+++ arrays/leetcode/238. Product of Array Except Self/main.java
@@ -0,0 +1,26 @@
+// package arrays.leetcode.238. Product of Array Except Self;
+
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int n = nums.length;
+        int[] res = new int[n];
+        int[] pref = new int[n];
+        int[] suff = new int[n];
+
+        pref[0] = 1;
+        suff[n - 1] = 1;
+        for (int i = 1; i < n; i++) {
+            pref[i] = nums[i - 1] * pref[i - 1];
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = nums[i + 1] * suff[i + 1];
+        }
+        for (int i = 0; i < n; i++) {
+            res[i] = pref[i] * suff[i];
+        }
+        System.out.println(Arrays.toString(res));;
+    }
+}
