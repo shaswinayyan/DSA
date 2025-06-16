8ba87c1b18bc9eb507b8bbdc3f2486d0f4fb075e
shaswinayyan <shaswinayyan@gmail.com>
2025-06-16 22:31:55 +0530

    optimal product array

--- arrays/leetcode/238. Product of Array Except Self/main.java
+++ arrays/leetcode/238. Product of Array Except Self/main.java
@@ -7,20 +7,18 @@ public class main {
         int[] nums = {1,2,3,4};
         int n = nums.length;
         int[] res = new int[n];
-        int[] pref = new int[n];
-        int[] suff = new int[n];
 
-        pref[0] = 1;
-        suff[n - 1] = 1;
+        res[0] = 1;
         for (int i = 1; i < n; i++) {
-            pref[i] = nums[i - 1] * pref[i - 1];
+            res[i] = res[i - 1] * nums[i - 1];
         }
-        for (int i = n - 2; i >= 0; i--) {
-            suff[i] = nums[i + 1] * suff[i + 1];
+        
+        int postfix = 1;
+        for (int i = n - 1; i >= 0; i--) {
+            res[i] *= postfix;
+            postfix *= nums[i];
         }
-        for (int i = 0; i < n; i++) {
-            res[i] = pref[i] * suff[i];
-        }
+        // return res;
         System.out.println(Arrays.toString(res));;
     }
 }
