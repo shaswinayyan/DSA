610220d9363e28cb5ea73c67440b5b8ab9ce9d2a
shaswinayyan <shaswinayyan@gmail.com>
2025-06-20 23:21:40 +0530

    right max 1299 leetcode

--- arrays/leetcode/1299. Replace Elements with Greatest Element on Right Side/main.java
+++ arrays/leetcode/1299. Replace Elements with Greatest Element on Right Side/main.java
@@ -1,14 +1,16 @@
 // package arrays.leetcode.1299. Replace Elements with Greatest Element on Right Side;
 
+import java.util.Arrays;
+
 public class main {
     public static void main(String[] args) {
         int[] arr ={17,18,5,4,6,1};
         int[] res = new int[arr.length];
-        for(int i=0; i<arr.length-1; i++){
-            
-            for(int j = i+1; j<arr.length; j++){
-                
-            }
+        int rightMax =-1;
+        for(int i=arr.length-1; i>=0; i--){
+            res[i] = rightMax;
+            rightMax = Math.max(arr[i], rightMax);
         }
+        System.out.println(Arrays.toString(res));
     }
 }
