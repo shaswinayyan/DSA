5c7529ae55640c55b66b76bc53ce4e914979cf60
shaswinayyan <shaswinayyan@gmail.com>
2025-06-28 22:51:47 +0530

    tried to create a hashmap

---
+++ arrays/leetcode/706. Design HashMap/main.java
@@ -0,0 +1,22 @@
+public class main {
+    public static void main(String[] args) {
+        private int[] map;
+
+        public MyHashMap() {
+            map = new int[1000001];
+            Arrays.fill(map, -1);
+        }
+    
+        public void put(int key, int value) {
+            map[key] = value;
+        }
+    
+        public int get(int key) {
+            return map[key];
+        }
+    
+        public void remove(int key) {
+            map[key] = -1;
+        }
+    }
+}
